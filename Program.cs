28ccfa649b2670ca28699c4f3498b8364bb583fe
Olivia Lind <eo.lind@gmail.com>
2022-06-03 10:22:48 -0500

    added files and pasted sample code from chapter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zero fxs = new Zero();
+            Tesla modelS = new Tesla();
+            Cessna mx410 = new Cessna();
+
+            fxs.Drive();
+            modelS.Drive();
+            mx410.Drive();
+        }
+    }
+}
